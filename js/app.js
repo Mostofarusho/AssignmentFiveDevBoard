70fb7f461b03eb2dcbc9ca08901642fd884ec6e2
MostofaFattha <mostofacse14@gmail.com>
2025-09-20 22:59:14 +0800

    Doing Card Section

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Bg Color
 const colors = ["#FF3811", "teal", "purple", "gold", "pink"];
 let index = 0;
 
@@ -5,3 +6,9 @@ document.getElementById("themeBtn").addEventListener("click", () => {
     document.documentElement.style.setProperty("--page-bg", colors[index]);
     index = (index + 1) % colors.length;
 });
+// Get today's date
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+const today = new Date().toLocaleDateString('en-US', options);
+
+// Insert into DOM
+document.getElementById("today").textContent = today;
