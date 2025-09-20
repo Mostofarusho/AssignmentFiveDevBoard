6c7daa188107aad66a04a43654382e48f5c683f4
MostofaFattha <mostofacse14@gmail.com>
2025-09-20 12:14:35 +0800

    NavBar Correction

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@ const colors = ["#FF3811", "teal", "purple", "gold", "pink"];
 let index = 0;
 
 document.getElementById("themeBtn").addEventListener("click", () => {
-    document.documentElement.style.backgroundColor = colors[index];
+    document.documentElement.style.setProperty("--page-bg", colors[index]);
     index = (index + 1) % colors.length;
 });
