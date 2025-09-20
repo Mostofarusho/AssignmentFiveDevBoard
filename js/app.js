b996efae66149ef27b8f305b1d286a5fcc9e29c8
MostofaFattha <mostofacse14@gmail.com>
2025-09-20 11:37:01 +0800

    NavBar Complete

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-console.log("hello");
+const colors = ["#FF3811", "teal", "purple", "gold", "pink"];
+let index = 0;
+
+document.getElementById("themeBtn").addEventListener("click", () => {
+    document.documentElement.style.backgroundColor = colors[index];
+    index = (index + 1) % colors.length;
+});
