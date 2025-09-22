6acf39368764c41b36482e6a69840da77f4b5aa1
MostofaFattha <mostofacse14@gmail.com>
2025-09-23 00:39:59 +0800

    Start JS

--- js/app.js
+++ js/app.js
@@ -12,3 +12,19 @@ const today = new Date().toLocaleDateString('en-US', options);
 
 // Insert into DOM
 document.getElementById("today").textContent = today;
+// Start My JS Code 
+// totaltask
+const totalTask = document.getElementById("total_task").innerText;
+console.log(totalTask);
+// taskAssigned
+const taskAssigned = document.getElementById("task_assigned").innerText;
+console.log(taskAssigned);
+// AllBtnAdd
+const all_btn = document.getElementsByClassName("add_btn");
+// console.log(all_btn);
+for (const btn of all_btn) {
+    btn.addEventListener("click", (event) => {
+        const name = event.target.parentNode.parentNode.childNodes[1].innerText;
+        console.log(name);
+    })
+}
