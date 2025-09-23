df31ff3d38396cb3ee3c21e97fde22d03d9f4f5f
MostofaFattha <mostofacse14@gmail.com>
2025-09-23 14:57:10 +0800

    Complete Project

--- js/app.js
+++ js/app.js
@@ -13,31 +13,50 @@ const today = new Date().toLocaleDateString('en-US', options);
 // Insert into DOM
 document.getElementById("today").textContent = today;
 // Start My JS Code 
-// totaltask
-const totalTask = document.getElementById("total_task").innerText;
-console.log(totalTask);
-// taskAssigned
-const taskAssigned = document.getElementById("task_assigned").innerText;
-console.log(taskAssigned);
+
 // AllBtnAdd
 const all_btn = document.getElementsByClassName("add_btn");
 // console.log(all_btn);
 for (const btn of all_btn) {
     btn.addEventListener("click", (event) => {
-        const name = event.target.parentNode.parentNode.childNodes[3].innerText;
-        console.log(name);
-        const selectedContainer = document.getElementById("selected-task-container");
-        // Time Task
-        // Get current time
-        const now = new Date();
-        const options = { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
-        const currentTime = now.toLocaleTimeString('en-US', options);
 
-        const div = document.createElement("div");
-        const p1 = document.createElement("p");
-        p1.innerHTML = `You Have Completed your task ${name} at ${currentTime} </br>`
-            // p1.innerText = name;
-        div.appendChild(p1);
-        selectedContainer.appendChild(div);
+        if (confirm(("Are you sure you want to complete this task?"))) {
+            // totaltask
+            const totalTask = document.getElementById("total_task").innerText;
+            const convertedTotalTask = parseInt(totalTask);
+            // taskAssigned
+            const taskAssigned = document.getElementById("task_assigned").innerText;
+            const convertedTaskAssigned = parseInt(taskAssigned);
+            document.getElementById("total_task").innerText = convertedTotalTask + 1;
+            document.getElementById("task_assigned").innerText = convertedTaskAssigned - 1;
+            const name = event.target.parentNode.parentNode.childNodes[3].innerText;
+            console.log(name);
+            const selectedContainer = document.getElementById("selected-task-container");
+            // Time Task
+            // Get current time
+            const now = new Date();
+            const options = { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
+            const currentTime = now.toLocaleTimeString('en-US', options);
+
+            const div = document.createElement("div");
+            const p1 = document.createElement("p");
+            p1.innerHTML = `You Have Completed your task ${name} at ${currentTime} </br>`
+                // p1.innerText = name;
+            div.appendChild(p1);
+            selectedContainer.appendChild(div);
+            btn.classList.add("disable");
+
+        } else {
+            alert("The Task is not complete yet");
+        }
     })
 }
+const clearButton = document.getElementById("activity_log_clear");
+clearButton.addEventListener("click", (event) => {
+    if (confirm("You really Want to clear your activity log")) {
+        const selectedContainer = document.getElementById("selected-task-container");
+        selectedContainer.innerText = "";
+    } else {
+        alert("Activity Log as it is ")
+    }
+})
